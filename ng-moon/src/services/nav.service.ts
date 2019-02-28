d9a3646e708645245d94b69fec21457e5c5622fd
ng-nest-moon <ngnest@163.com>
2019-02-28 23:09:53 +0800

    解决表单上取消返回历史的问题

--- ng-moon/src/services/nav.service.ts
+++ ng-moon/src/services/nav.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { SimpleReuseStrategy } from 'src/main/simple-reuse-srategy';
 import { Location } from '@angular/common';
-import { Router, NavigationStart } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Injectable({ providedIn: 'root' })
 export class NavService {
 
   clearTo: boolean = false;
+
+  history: NavigationEnd[] = [];
 
   constructor(
     private router: Router,
@@ -20,17 +23,47 @@ export class NavService {
           this.clearTo = false;
         }
       })
+    this.router.events.pipe(filter(x => x instanceof NavigationEnd))
+      .subscribe((x: NavigationEnd) => {
+        if (x.url === x.urlAfterRedirects) this.history.unshift(x);
+      })
   }
 
   back(clearTo?: boolean) {
     this.removeThis();
     this.clearTo = clearTo;
-    history.back();
+    let first = _.first(this.history);
+    let urlSplit = _.split(first.url, '/');
+    if (urlSplit.length >= 3) {
+      let url = _.join(_.take(urlSplit, 3), '/');
+      let his = _.find(this.history, (x: NavigationEnd) =>
+        x.id !== first.id
+        && x.url.indexOf(url) === 0
+        && x.url !== first.url);
+      if (his) {
+        this.router.navigateByUrl(his.url).then(() => {
+          _.remove(this.history, x => x.id > his.id)
+        });
+      } else {
+        let url = _.join(_.take(urlSplit, urlSplit.length - 1), '/');
+        this.backTo(url, first.url);
+      }
+      return false;
+    }
+    window.history.back();
+  }
+
+  backTo(url, nowUrl) {
+    this.router.navigateByUrl(url).then(() => {
+      if (_.first(this.history).urlAfterRedirects === nowUrl) {
+        let urlSplit = _.split(url, '/');
+        this.backTo(_.join(_.take(urlSplit, urlSplit.length - 1), '/'), nowUrl)
+      }
+    })
   }
 
   removeThis() {
     let url = this.location.path();
-    // console.log(SimpleReuseStrategy.handlers);
     SimpleReuseStrategy.deleteRouteSnapshot(url);
   }
 }
